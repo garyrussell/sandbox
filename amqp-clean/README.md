ff4f280a745f91ad92203c9bd2b7989d25168065
Gary Russell <grussell@pivotal.io>
2016-09-02 17:02:31 -0400

    amqp-clean to Boot 1.4

---
+++ amqp-clean/README.md
@@ -0,0 +1,1 @@
+Cleans all exchanges and queues from RabbitMQ on http://localhost:15672
--- amqp-clean/src/main/java/org/springframework/amqpclean/AmqpCleanApplication.java
+++ amqp-clean/src/main/java/org/springframework/amqpclean/AmqpCleanApplication.java
@@ -1,31 +1,22 @@
 package org.springframework.amqpclean;
 
-import java.util.List;
-
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.util.StringUtils;
 
 import com.rabbitmq.http.client.Client;
-import com.rabbitmq.http.client.domain.ExchangeInfo;
-import com.rabbitmq.http.client.domain.QueueInfo;
 
 @SpringBootApplication
 public class AmqpCleanApplication {
 
 	public static void main(String[] args) throws Exception {
-//		ConfigurableApplicationContext ctx = SpringApplication.run(AmqpCleanApplication.class, args);
 		Client client = new Client("http://localhost:15672/api/", "guest", "guest");
-		List<ExchangeInfo> exchanges = client.getExchanges();
-		for (ExchangeInfo info : exchanges) {
-			if (info.getName().startsWith("xdbus")
-					|| (StringUtils.hasText(info.getName()) && !info.getName().startsWith("amq."))) {
-				client.deleteExchange("/", info.getName());
-			}
-		}
-		List<QueueInfo> queues = client.getQueues();
-		for (QueueInfo info : queues) {
-			client.deleteQueue("/", info.getName());
-		}
+		client.getExchanges()
+				.stream()
+				.filter(ei -> StringUtils.hasText(ei.getName()) && !ei.getName().startsWith("amq."))
+				.forEach(ei -> client.deleteExchange("/", ei.getName()));
+		client.getQueues()
+				.stream()
+				.forEach(qi -> client.deleteQueue("/", qi.getName()));
 	}
 
 }
