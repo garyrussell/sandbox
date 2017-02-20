05c7f9611d3582223a8fd99b4a112c7c791f7313
Gary Russell <grussell@pivotal.io>
2017-02-20 15:58:34 -0500

    Custom TCP Source
    so42346872

---
+++ so42346872/src/main/java/com/example/So42346872Application.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class So42346872Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(So42346872Application.class, args);
+	}
+}
---
+++ so42346872/src/main/java/com/example/TcpSourceProperties.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright 2015 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.example;
+
+import javax.validation.constraints.NotNull;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.cloud.stream.app.tcp.AbstractTcpConnectionFactoryProperties;
+
+/**
+ * Properties for the TCP Source.
+ *
+ * @author Gary Russell
+ *
+ */
+@ConfigurationProperties("tcp")
+public class TcpSourceProperties extends AbstractTcpConnectionFactoryProperties {
+
+	/**
+	 * The decoder to use when receiving messages.
+	 */
+	private Encoding decoder = Encoding.CRLF;
+
+	/**
+	 * The buffer size used when decoding messages; larger messages will be rejected.
+	 */
+	private int bufferSize = 2048;
+
+	@NotNull
+	public Encoding getDecoder() {
+		return this.decoder;
+	}
+
+	public void setDecoder(Encoding decoder) {
+		this.decoder = decoder;
+	}
+
+	public int getBufferSize() {
+		return bufferSize;
+	}
+
+	public void setBufferSize(int bufferSize) {
+		this.bufferSize = bufferSize;
+	}
+
+}
